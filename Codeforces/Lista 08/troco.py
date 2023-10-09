53ed6a30be64b6c799b4f48eeb20d3752b7a1d1f
Victor Passos <131535916+vSteps@users.noreply.github.com>
2023-10-09 10:15:20 -0300

    Exercícios Codeforecs

---
+++ Codeforces/Lista 08/troco.py
@@ -0,0 +1,13 @@
+def maior_troco(preco_marcas, din):
+    for i in range(len(preco_marcas)):
+        if preco_marcas[i] > din:
+            preco_marcas[i] = 0
+        else:
+            preco_marcas[i] = din % preco_marcas[i]
+    return max(preco_marcas)
+ 
+x = int(input())
+for i in range(x):
+    din, qnt_marc = map(int, input().split())
+    marcas = list(map(float, input().split()))
+    print(f"{maior_troco(marcas, din):.2f}")
