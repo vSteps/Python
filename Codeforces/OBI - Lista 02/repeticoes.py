3db6b7659177e3fcc180d6696a4c5a2c13f1efdb
Victor Passos <131535916+vSteps@users.noreply.github.com>
2023-10-11 10:02:46 -0300

    Exercícios Codeforces

---
+++ Codeforces/OBI - Lista 02/repeticoes.py
@@ -0,0 +1,14 @@
+def repetição(seq):
+    rep_max = 1
+    rep_atual = 1
+    for i in range(1,len(seq)):
+        if seq[i]==seq[i-1]:
+            rep_atual +=1
+        else:
+            rep_atual = 1
+        rep_max = max(rep_max, rep_atual)
+    return rep_max
+
+
+dna = input()
+print(repetição(dna))
