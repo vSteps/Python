e38753318cab7dac203f10ee961cc02dd77772a5
Victor Passos <131535916+vSteps@users.noreply.github.com>
2023-10-11 10:03:11 -0300

    Exercícios Codeforces

---
+++ Codeforces/OBI - Lista 02/fita_colorida.py
@@ -0,0 +1,19 @@
+N = int(input())
+ 
+fita = [int(x) for x in input().split()]
+fita[N:] = [-1]
+fita[:0] = [-1] 
+ 
+for c in range(8):
+    for i in range(1, N+1):
+        if (fita[i] == -1):
+            if (fita[i-1] == c or fita[i+1] == c):
+                fita[i] = c + 1
+ 
+for i in range(1, N+1):
+    if (fita[i] == -1):
+        fita[i] = 9
+ 
+for i in range(1, N):
+    print(fita[i], end=' ')
+print(fita[N])
